41aedff203a4755918752c98ff2a24594485ed25
avivikas <110584113+avivikas@users.noreply.github.com>
2022-08-04 07:54:14 -0700

    Add files via upload

---
+++ mydata.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+
+
+# In[2]:
+
+
+os.getcwd()
+
+
+# In[3]:
+
+
+import pandas as pd
+
+
+# In[5]:
+
+
+pd.read_csv("mydata.tsv",sep="\t")
+
+
+# In[ ]:
+
+
+
+
